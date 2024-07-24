a9762d5a589cbc1b9f7816699b9fb136446a9c50
Rinaldin Muhamad Rizky <rinaldinrizky@gmail.com>
2024-07-25 05:02:23 +0700

    1

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost"; // ganti dengan MySQL Host yang benar dari hPanel
+$username = "u126967367_login"; // username MySQL dari hPanel
+$password = "Aldingans123456"; // password MySQL yang benar
+$dbname = "u126967367_login"; // nama database
+
+// Membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
