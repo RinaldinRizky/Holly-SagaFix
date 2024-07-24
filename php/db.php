b3cb2d8f52dbc4c3920248c2cad733ce0d7909ee
Rinaldin Muhamad Rizky <rinaldinrizky@gmail.com>
2024-07-24 09:00:50 +0700

    update database

--- php/db.php
+++ php/db.php
@@ -1,7 +1,14 @@
-<?php 
+<?php
+$host = 'localhost'; // Biasanya 'localhost' untuk server shared hosting
+$username = 'u126967367_userforms'; // Ganti dengan nama pengguna database Anda
+$password = 'Aldingans123'; // Ganti dengan password database Anda
+$database = 'u126967367_userforms'; // Ganti dengan nama database Anda
 
-$conn = new mysqli('localhost','root','','usersform');
-if(!$conn){
-    echo "Connection Denied!" . mysqli_connect_error();
-} 
+// Membuat koneksi menggunakan MySQLi
+$conn = new mysqli($host, $username, $password, $database);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
 ?>
